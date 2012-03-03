8163c8db8089fe39e6ef1e10c2b2329e45c758e0
Lucas Caballero <caballero.luke@gmail.com>
2012-03-03 16:39:21 -0700

    Added 'Web App - Performance Tuning'

---
+++ Scripts/PageManager.js
@@ -0,0 +1,17 @@
+﻿;(function ($) {
+
+	$.ajax({
+		url:"Writing/Performance-Tuning.html",
+		method:"GET",
+		data:{},
+		success:function( result ) {
+			console.info( result );
+			result = $(result);
+			console.info( result );
+			$("#FullContent").append( $(result) );
+		},
+		error:function( result ) {
+		}
+	});
+
+})(jQuery);
